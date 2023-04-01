8b96ad44545a391819858f59787e605df400153e
PolinaB <polina@mail.ru>
2023-04-01 12:32:40 +0300

    Initial commit

---
+++ geo.py
@@ -0,0 +1,65 @@
+import math
+import requests
+
+apikey = "40d1649f-0493-4b70-98ba-98533de7710b"
+
+
+def get_coordinates(city_name):
+    try:
+        # url, по которому доступно API Яндекс.Карт
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        # параметры запроса
+        params = {
+            'apikey': apikey,
+            # город, координаты которого мы ищем
+            'geocode': city_name,
+            # формат ответа от сервера, в данном случае JSON
+            'format': 'json'
+
+        }
+        # отправляем запрос
+        response = requests.get(url, params)
+        # получаем JSON ответа
+        json = response.json()
+        # получаем координаты города (там написаны долгота(longitude),
+        # широта(latitude) через пробел).
+        # Посмотреть подробное описание JSON-ответа можно
+        # в документации по адресу
+        # https://tech.yandex.ru/maps/geocoder/
+        coordinates_str = json['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['Point']['pos']
+        # Превращаем string в список, так как точка -
+        # это пара двух чисел - координат
+        long, lat = map(float, coordinates_str.split())
+        # Вернем ответ
+        return long, lat
+    except Exception as e:
+        return e
+
+
+def get_country(city_name):
+    import requests
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            'apikey': apikey,
+            'geocode': city_name,
+            'format': 'json'
+        }
+        data = requests.get(url, params).json()
+        # все отличие тут, мы получаем имя страны
+        return data['response']['GeoObjectCollection']['featureMember'][0][
+            'GeoObject']['metaDataProperty']['GeocoderMetaData'][
+            'AddressDetails']['Country']['CountryName']
+    except Exception as e:
+        return 'Возникла ошибка! Можно только понять и простить :('
+
+
+def get_geo_info(city_name, type_info):
+    if type_info == 'country':
+        return get_country(city_name)
+    elif type_info == 'coordinates':
+        return get_coordinates(city_name)
+    else:
+        return "Кажется, вы указали неверный параметр"
+
